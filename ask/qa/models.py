e9dfbdde455267892709aac9f81f9b632cd8f204
Sergey Shuktomov <s_shuktomov@mail.ru>
2020-07-03 19:28:44 +0300

    comment

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,24 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+      return self.order_by('-added_at')
+    def popular(self):
+      return self.order_by('-rating')
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    author = models.CharField()
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    author = models.CharField()
+    question = models.OneToOneField(Question)
+    
--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo service gunicorn restart
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+~/web/ask/manage.py makemigrations
+~/web/ask/manage.py migrate
