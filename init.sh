2d2de4550b2cf62ba6515e45ca9fad2b5ab38df5
Sergey Shuktomov <s_shuktomov@mail.ru>
2020-07-05 00:07:10 +0300

    comment

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@ mysql -uroot -e "create database stepic_web;"
 mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
 ~/web/ask/manage.py makemigrations
 ~/web/ask/manage.py migrate
+sudo ln -sf /home/box/web/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo service gunicorn restart
