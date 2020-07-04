c2519c18a7052368ad24b4fc5ee09590628b546c
typhoonseryi <66803009+typhoonseryi@users.noreply.github.com>
2020-07-05 00:54:57 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -20,5 +20,5 @@ class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User)
-    answer_set = models.OneToOneField(Question)
+    question = models.OneToOneField(Question)
     
